d24c59ab7e8aa5f2c9b855cf65d962a2d72ad851
Ondřej Hlaváč <info@bluepro.cz>
2008-07-19 18:45:15 +0000

    Oprava chyby se zdvojenými titulky ve verzi 1.5.4

--- joomla/components/com_content/views/article/tmpl/default.php
+++ joomla/components/com_content/views/article/tmpl/default.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die('Restricted access');
 
 $canEdit	= ($this->user->authorize('com_content', 'edit', 'content', 'all') || $this->user->authorize('com_content', 'edit', 'content', 'own'));
 ?>
-<?php if ($this->params->get('show_page_title', 1)) : ?>
+<?php if ($this->params->get('show_page_title', 0)) : ?>
 <div class="componentheading<?php echo $this->params->get('pageclass_sfx')?>"><?php echo $this->escape($this->params->get('page_title')); ?></div>
 <?php endif; ?>
 <?php if ($canEdit || $this->params->get('show_title') || $this->params->get('show_pdf_icon') || $this->params->get('show_print_icon') || $this->params->get('show_email_icon')) : ?>
