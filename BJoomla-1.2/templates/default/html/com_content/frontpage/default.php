3028400d9028f934f24920a39b75c57e3e25070a
Ondřej Hlaváč <info@bluepro.cz>
2009-03-30 08:33:20 +0000

    BJoomla 1.2 - Joomla 1.5.10

---
+++ BJoomla-1.2/templates/default/html/com_content/frontpage/default.php
@@ -0,0 +1,46 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+echo sprintf('<div class="com_content%s">', $this->params->get('pageclass_sfx'));
+
+if ($this->params->get('show_page_title', 0)) {
+	echo sprintf('<h1>%s</h1>', $this->escape($this->params->get('page_title')));
+}
+
+if ($this->params->def('num_leading_articles', 1)) {
+	for ($i = $this->pagination->limitstart; $i < ($this->pagination->limitstart + $this->params->get('num_leading_articles')) && $i < $this->total; $i++) {
+		$this->item = $this->getItem($i, $this->params);
+		echo $this->loadTemplate('item');
+	}
+} else {
+	$i = $this->pagination->limitstart;
+}
+
+$intro_articles = $this->params->get('num_intro_articles', 4);
+if ($intro_articles) {
+	$columns = $this->params->def('num_columns', 2);
+	if (!$columns) $columns = 1;
+	$rows = ceil($intro_articles / $columns);
+	for ($j = 0; $j < $columns && $i < $this->total; $j++) {
+		echo sprintf('<div class="box_column%s">', $this->params->get('pageclass_sfx'));
+		for ($k = 0; $k < $rows && $i < $this->total; $k++, $i++) {
+			$this->item =& $this->getItem($i, $this->params);
+			echo $this->loadTemplate('item');
+		}
+		echo '</div>';
+	}
+}
+
+$links = $this->params->def('num_links', 4);
+if ($links && $i < $this->total) {
+	$this->links = array_slice($this->items, $i - $this->pagination->limitstart, $i - $this->pagination->limitstart + $links);
+	echo $this->loadTemplate('links');
+}
+
+if ($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2 && $this->pagination->get('pages.total') > 1)) {
+	echo $this->pagination->getPagesLinks();
+	if ($this->params->def('show_pagination_results', 1)) {
+		echo $this->pagination->getPagesCounter();
+	}
+}
+echo '</div>';
---
+++ BJoomla-1.2/templates/default/html/com_search/search/default_form.php
@@ -0,0 +1,42 @@
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+
+<form id="searchForm" name="searchForm" action="<?php echo JRoute::_( 'index.php?option=com_search' ); ?>" method="post">
+<div>
+	<input type="hidden" name="task" value="search" />
+	<fieldset>
+		<label for="search_searchword"><?php echo JText::_('Search Keyword'); ?></label>
+		<input type="text" name="searchword" id="search_searchword"  maxlength="20" value="<?php echo $this->escape($this->searchword); ?>" />
+		<input type="submit" name="Search" value="<?php echo JText::_( 'Search' );?>" />
+	</fieldset>
+
+	<fieldset>
+		<legend><?php echo JText::_('Search Parameters'); ?></legend>
+		<?php echo $this->lists['searchphrase']; ?>
+		<label for="ordering"><?php echo JText::_('Ordering'); ?></label>
+		<?php echo $this->lists['ordering']; ?>
+	</fieldset>
+
+<?php if ($this->params->get('search_areas', 1)) { ?>
+	<fieldset>
+		<legend><?php echo JText::_('Search Only'); ?></legend>
+<?php
+	foreach ($this->searchareas['search'] as $val => $txt) {
+		$checked = is_array($this->searchareas['active']) && in_array($val, $this->searchareas['active']) ? ' checked="true"' : '';
+?>
+		<input type="checkbox" name="areas[]" value="<?php echo $val ?>" id="area_<?php echo $val ?>"<?php echo $checked ?> />
+		<label for="area_<?php echo $val ?>"><?php echo JText::_($txt); ?></label>
+<?php } ?>
+	</fieldset>
+<?php
+}
+
+if ($this->total > 1) {
+?>
+	<label for="limit"><?php echo JText :: _('Display Num') ?></label>
+<?php
+	echo $this->pagination->getLimitBox();
+	echo $this->pagination->getPagesCounter();
+}
+?>
+</div>
+</form>
