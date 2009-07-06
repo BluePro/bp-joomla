850bbbb09624472af8de1e08db255563318bd685
Ondřej Hlaváč <info@bluepro.cz>
2009-07-06 19:27:12 +0000

    Úprava defaultní šablony.

--- joomla/templates/default/index.php
+++ joomla/templates/default/index.php
@@ -1,45 +1,52 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+$config = new JConfig();
+$user = JFactory::getUser();
+	
+if ($user->get('guest') == 1 or $user->usertype == 'Registered') {
+	$header = $this->getHeadData();
+	$scripts = array();
+	foreach ($header['scripts'] as $script => $type) {
+		if ($script != '/media/system/js/mootools.js' && $script != '/media/system/js/caption.js') {
+			$scripts[$script] = $type;
+		}
+	}
+	$header['scripts'] = $scripts;
+	$this->setHeadData($header);
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>">
 <head>
 	<jdoc:include type="head" />
-	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/baby/css/screen.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/default/css/screen.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/default/css/content.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/default/css/print.css" type="text/css" media="print" />
+<!--[if lte IE 6]>
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/default/css/ie6.css" type="text/css" media="screen" />
+<![endif]-->
+	<link rel="shortcut icon" href="<?php echo $this->baseurl ?>/templates/default/images/favicon.gif" />
 </head>
+
 <body>
-	<div id="header"><jdoc:include type="modules" name="top" /></div>
-	<div id="content">
-<?php if ($this->countModules('user1')) : ?>
 	<div id="logo">
-		<h1><span>Vojtíšek Štěpaník</span></h1>
-		<div>
-			Dnes je mi <span>
 <?php
-	// Vypocet veku
-	$od = mktime(0, 0, 0, 6, 4, 2008);
-    $do = time();
-    $od_l = date('I', $od);
-    $do_l = date('I', $do);
-
-    if ($do_l > $od_l) $do += 60 * 60;
-    elseif ($do_l < $od_l) $od += 60 * 60;
-
-    $diff = floor(($do - $od) / (24 * 60 * 60));
-    echo $diff;
+	if ($this->countModules('logo')) {
 ?>
-			</span> dní!
-		</div>
+		<jdoc:include type="modules" name="logo" /> 
+<?php
+	} else {
+		echo $config->sitename;
+	}
+?>
 	</div>
-	<jdoc:include type="modules" name="user1" />
-	<div class="cleaner">&nbsp;</div>
-<?php endif; ?>
-	<jdoc:include type="component" />
+	<div id="top"><jdoc:include type="modules" name="top" /></div>
+	<div id="left"><jdoc:include type="modules" name="left" style="header" /></div>
+	<div id="content">
+		<jdoc:include type="message" />
+		<jdoc:include type="component" />
 	</div>
-	<div id="footer">
-		<div class="container">
-			<div id="toy_01"></div>
-			<jdoc:include type="modules" name="bottom" />
-			<div id="toy_02"></div>
-		</div>
-	</div>
+	<div id="bottom"><jdoc:include type="modules" name="bottom" /></div>
 </body>
 </html>
