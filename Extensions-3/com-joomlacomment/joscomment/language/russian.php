71a866fca047fc7943f85a5a3ff8a32c46f59ceb
Ondřej Hlaváč <info@bluepro.cz>
2009-08-05 06:12:44 +0000

    Upgrade phocagallery na 2.5.5

---
+++ Extensions-3/com-joomlacomment/joscomment/language/russian.php
@@ -0,0 +1,151 @@
+﻿<?php
+// thanks to Alexey
+// thanks to Zevgma http://www.creativity-photo.com
+
+DEFINE('_JOOMLACOMMENT_WRITECOMMENT', 'Комментарии');
+DEFINE('_JOOMLACOMMENT_EDITCOMMENT', 'Править комментарий');
+DEFINE('_JOOMLACOMMENT_UBBCODE', 'UBB-Код:');
+DEFINE('_JOOMLACOMMENT_ENTERNAME', 'Имя:');
+DEFINE('_JOOMLACOMMENT_ENTERNOTIFY0', 'не уведомлять');
+DEFINE('_JOOMLACOMMENT_ENTERNOTIFY1', 'уведомлять');
+DEFINE('_JOOMLACOMMENT_NOTIFYTXT1', 'уведомлять о новом комментарии');
+DEFINE('_JOOMLACOMMENT_NOTIFYTXT0', 'не уведомлять о новом комментарии');
+DEFINE('_JOOMLACOMMENT_ENTEREMAIL', 'E-mail:');
+DEFINE('_JOOMLACOMMENT_AUTOMATICEMAIL', 'автоматически');
+DEFINE('_JOOMLACOMMENT_ENTERWEBSITE', 'Веб-сайт:');
+DEFINE('_JOOMLACOMMENT_ENTERTITLE', 'Заголовок:');
+DEFINE('_JOOMLACOMMENT_SENDFORM', 'Отправить');
+DEFINE('_JOOMLACOMMENT_BY', ' \ ');
+
+DEFINE('_JOOMLACOMMENT_COLOR', 'цвет');
+DEFINE('_JOOMLACOMMENT_AQUA', 'морская волна (aqua)');
+DEFINE('_JOOMLACOMMENT_BLACK', 'чёрный (black)');
+DEFINE('_JOOMLACOMMENT_BLUE', 'голубой (blue)');
+DEFINE('_JOOMLACOMMENT_FUCHSIA', 'фуксия (fuchsia)');
+DEFINE('_JOOMLACOMMENT_GRAY', 'серый (gray)');
+DEFINE('_JOOMLACOMMENT_GREEN', 'зелёный (green)');
+DEFINE('_JOOMLACOMMENT_LIME', 'лайм (lime)');
+DEFINE('_JOOMLACOMMENT_MAROON', 'красно-коричневый (maroon)');
+DEFINE('_JOOMLACOMMENT_NAVY', 'тёмно-синий (navy)');
+DEFINE('_JOOMLACOMMENT_OLIVE', 'оливковый (olive)');
+DEFINE('_JOOMLACOMMENT_PURPLE', 'пурпурный (purple)');
+DEFINE('_JOOMLACOMMENT_RED', 'красный (red)');
+DEFINE('_JOOMLACOMMENT_SILVER', 'серебрянный (silver)');
+DEFINE('_JOOMLACOMMENT_TEAL', 'бирюзовый (teal)');
+DEFINE('_JOOMLACOMMENT_WHITE', 'белый (white)');
+DEFINE('_JOOMLACOMMENT_YELLOW', 'жёлтый (yellow)');
+
+DEFINE('_JOOMLACOMMENT_SIZE', 'размер');
+DEFINE('_JOOMLACOMMENT_TINY', 'крошечный (x-small)');
+DEFINE('_JOOMLACOMMENT_SMALL', 'маленький (small)');
+DEFINE('_JOOMLACOMMENT_MEDIUM', 'средний (medium)');
+DEFINE('_JOOMLACOMMENT_LARGE', 'большой (large)');
+DEFINE('_JOOMLACOMMENT_HUGE', 'огромный (x-large)');
+
+DEFINE('_JOOMLACOMMENT_QUOTE', 'Цитата');
+DEFINE('_JOOMLACOMMENT_REPLY', 'Ответить');
+DEFINE('_JOOMLACOMMENT_EDIT', 'Изменить');
+DEFINE('_JOOMLACOMMENT_DELETE', 'Удалить');
+
+DEFINE('_JOOMLACOMMENT_UBB_WROTE', 'писал:');
+DEFINE('_JOOMLACOMMENT_UBB_QUOTE', 'цитата:');
+DEFINE('_JOOMLACOMMENT_UBB_CODE', 'код:');
+
+DEFINE('_JOOMLACOMMENT_FORMVALIDATE', 'Пожалуйста, введите, хотя бы, Ваш комментарий.');
+DEFINE('_JOOMLACOMMENT_FORMVALIDATE_EMAIL', 'Для получения уведомления, пожалуйста, введите свой E-mail');
+DEFINE('_JOOMLACOMMENT_FORMVALIDATE_CAPTCHA', 'Пожалуйста, введите проверочный код, который Вы видите на картинке.');
+DEFINE('_JOOMLACOMMENT_FORMVALIDATE_CAPTCHATXT', 'Пожалуйста, введите проверочный код, который Вы видите на картинке.');
+DEFINE('_JOOMLACOMMENT_FORMVALIDATE_CAPTCHA_FAILED', 'Проверочный код введён не верно. Пожалуйста, введите код, который Вы видите на картинке.');
+DEFINE('_JOOMLACOMMENT_MSG_DELETE', 'Вы уверены, что хотите удалить этот комментарий?');
+DEFINE('_JOOMLACOMMENT_SAVINGFAILED', 'Возникла ошибка при отправке комментария!');
+DEFINE('_JOOMLACOMMENT_EDITINGFAILED', 'Возникла ошибка при редактировании комментария!');
+DEFINE('_JOOMLACOMMENT_DELETINGFAILED', 'Возникла ошибка при удалении комментария!');
+DEFINE('_JOOMLACOMMENT_REQUEST_ERROR','Запрос не удался');
+
+DEFINE('_JOOMLACOMMENT_ONLYREGISTERED', 'Только зарегистрированные пользователи могут оставлять комментарии!');
+DEFINE('_JOOMLACOMMENT_ANONYMOUS', 'Анонимно');
+
+DEFINE('_JOOMLACOMMENT_ADDNEW', 'Добавить новый');
+DEFINE('_JOOMLACOMMENT_DELETEALL', 'Удалить все');
+DEFINE('_JOOMLACOMMENT_MSG_DELETEALL', 'Вы уверены, что хотите удалить все комментарии?');
+DEFINE('_JOOMLACOMMENT_RSS', 'RSS');
+
+DEFINE('_JOOMLACOMMENT_SEARCH', 'Поиск');
+DEFINE('_JOOMLACOMMENT_PROMPT_KEYWORD', 'Искать слова');
+DEFINE('_JOOMLACOMMENT_SEARCH_ANYWORDS', 'любое из');
+DEFINE('_JOOMLACOMMENT_SEARCH_ALLWORDS', 'все');
+DEFINE('_JOOMLACOMMENT_SEARCH_PHRASE', 'точное совпадение');
+DEFINE('_JOOMLACOMMENT_NOSEARCHMATCH', 'Нет ни тем, ни сообщений, удовлетворяющих критериям поиска.');
+DEFINE('_JOOMLACOMMENT_SEARCHMATCH', 'Найдено по запросу позиций: %d.');
+DEFINE('_JOOMLACOMMENT_SEARCHMATCHES', 'Найдено по запросу позиций: %d совпадений.');
+
+DEFINE('_JOOMLACOMMENT_BEFORE_APPROVAL', 'Ваш комментарий принят к рассмотрению администраторами сайта и будет опубликован в случае одобрения ими.');
+/* -----------------------------------------------------------------------------
+ * NEW OR MODIFIED IN THE 3.20
+ * 
+ */
+JOSC_define('_JOOMLACOMMENT_COMMENTS_TITLE', 'Комментарии'); 	/* Will replace {_COMMENTS_2_4} in template */ 
+JOSC_define('_JOOMLACOMMENT_COMMENTS_0', ''); 		/* for Read ON: 0 */ 
+JOSC_define('_JOOMLACOMMENT_COMMENTS_1', ''); 	/* for Read ON: when only one comment */
+JOSC_define('_JOOMLACOMMENT_COMMENTS_2_4', ''); 	/* for Read ON: 2 to 4 comments */
+JOSC_define('_JOOMLACOMMENT_COMMENTS_MORE', ''); /* for Read ON: more than 4 */ 
+
+JOSC_define('_JOOMLACOMMENT_EMPTYCOMMENT', 'Вы не ввели текст');
+
+JOSC_define('_JOOMLACOMMENT_NOTIFY_NEW_SUBJECT', 'NewComment:{title} [from:{name}][notify:{notify}]');
+JOSC_define('_JOOMLACOMMENT_NOTIFY_NEW_MESSAGE',		'<p>A user has posted a new comment to a content item you have subscribed <br />in <a href="{livesite}">{livesite}</a>:</p>'
+											. 	'<p><b>Имя: </b>{name}<br />'
+											. 	'<b>Заголовок: </b>{title}<br />'
+											. 	'<b>Текст: </b>{comment}<br />'
+											. 	'<b>Содержимое: </b><a href="{linkURL}">{linkURL}</a></p>'
+											. 	'<p>Пожалуйста не отвечайте на данное сообщение, оно служит только для информирования.</p>'
+											. 	'<p>Отказаться от подписки :<br />'
+											.	'- если вы зарегистрированный пользователь : то зайдите и отредактируйте<br />'
+											.	'- если вы не зарегистрированный пользователь : то данные настройки вам не доступны</p>'
+											);
+JOSC_define('_JOOMLACOMMENT_NOTIFY_TOBEAPPROVED_SUBJECT', 'ToBeApproved:{title} [from:{name}][notify:{notify}]');
+JOSC_define('_JOOMLACOMMENT_NOTIFY_TOBEAPPROVED_MESSAGE', '<p>Новый комментарий ожидает одобрения <br /> <a href="{livesite}">{livesite}</a>:</p>'
+											. 	'<p><b>Имя: </b>{name}<br />'
+											. 	'<b>Заголовок: </b>{title}<br />'
+											. 	'<b>Текст: </b>{comment}<br />'
+											. 	'<b>Содержимое: </b><a href="{linkURL}">{linkURL}</a></p>'
+											. 	'<p>Пожалуйста не отвечайте на данное сообщение, оно служит только для информирования.</p>'
+											);
+JOSC_define('_JOOMLACOMMENT_NOTIFY_EDIT_SUBJECT', 'EditedComment:{title} [from:{name}][notify:{notify}]');
+JOSC_define('_JOOMLACOMMENT_NOTIFY_EDIT_MESSAGE',	'<p>Пользователь отредактировал комментарий на которые вы подписаны <br /> {livesite}:</p>'
+											. 	'<p><b>Имя: </b>{name}<br />'
+											. 	'<b>Заголовок: </b>{title}<br />'
+											. 	'<b>Текст: </b>{comment}<br />'
+											. 	'<b>Содержимое: </b><a href="{linkURL}">{linkURL}</a></p>'
+											. 	'<p>Пожалуйста не отвечайте на данное сообщение, оно служит только для информирования.</p>'
+											. 	'<p>Отказаться от подписки :<br />'
+											.	'- если вы зарегистрированный пользователь : то зайдите и отредактируйте<br />'
+											.	'- если вы не зарегистрированный пользователь : то данные настройки вам не доступны</p>'
+											);
+JOSC_define('_JOOMLACOMMENT_NOTIFY_PUBLISH_SUBJECT', 'PublishedComment:{title} has been published');
+JOSC_define('_JOOMLACOMMENT_NOTIFY_PUBLISH_MESSAGE','<p>Следующий комментарий был опубликован <br /> <a href="{livesite}">{livesite}</a>:</p>'
+											. 	'<p><b>Имя: </b>{name}<br />'
+											. 	'<b>Заголовок: </b>{title}<br />'
+											. 	'<b>Текст: </b>{comment}<br />'
+											. 	'<b>Содержимое: </b><a href="{linkURL}">{linkURL}</a></p>'
+											. 	'<p>Пожалуйста не отвечайте на данное сообщение, оно служит только для информирования.</p>'
+											);        
+JOSC_define('_JOOMLACOMMENT_NOTIFY_UNPUBLISH_SUBJECT', 'UnpublishedComment:{title} has been UNpublished');
+JOSC_define('_JOOMLACOMMENT_NOTIFY_UNPUBLISH_MESSAGE','<p>Следующий комментарий не был опубликован <br />from <a href="{livesite}">{livesite}</a>:</p>'
+											. 	'<p><b>Имя: </b>{name}<br />'
+											. 	'<b>Заголовок: </b>{title}<br />'
+											. 	'<b>Текст: </b>{comment}<br />'
+											. 	'<b>Содержимое: </b><a href="{linkURL}">{linkURL}</a></p>'
+											. 	'<p>Пожалуйста не отвечайте на данное сообщение, оно служит только для информирования.</p>'
+											);        
+JOSC_define('_JOOMLACOMMENT_NOTIFY_DELETE_SUBJECT', 'DeletedComment:{title} был удален');
+JOSC_define('_JOOMLACOMMENT_NOTIFY_DELETE_MESSAGE','<p>Следующий комментарий был удален <br /> <a href="{livesite}">{livesite}</a>:</p>'
+											. 	'<p><b>Имя: </b>{name}<br />'
+											. 	'<b>Заголовок: </b>{title}<br />'
+											. 	'<b>Текст: </b>{comment}<br />'
+											. 	'<b>Содержимое: </b><a href="{linkURL}">{linkURL}</a></p>'
+											. 	'<p>Пожалуйста не отвечайте на данное сообщение, оно служит только для информирования.</p>'
+											);    
+JOSC_define('_JOOMLACOMMENT_MSG_NEEDREFRESH', '' );
+JOSC_define('_JOOMLACOMMENT_RELOAD_CAPTCHA', 'обновить изображение');
+?>
