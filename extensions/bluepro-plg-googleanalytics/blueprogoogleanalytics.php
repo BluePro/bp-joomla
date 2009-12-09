2c611969434b2a003138e8ac0e27be6539e10c6d
Ondřej Hlaváč <info@bluepro.cz>
2009-12-09 12:07:50 +0000

    Oprava Google Analytics

--- extensions/bluepro-plg-googleanalytics/blueprogoogleanalytics.php
+++ extensions/bluepro-plg-googleanalytics/blueprogoogleanalytics.php
@@ -9,7 +9,7 @@ class plgSystemBlueprogoogleanalytics extends JPlugin {
 		$tracker = $this->params->get('tracker', '');
 		$app =& JFactory::getApplication();
 		
-		if (!$tracker || $app->isAdmin()) return true;
+		if (!$tracker || $app->isAdmin() || strpos($_SERVER['PHP_SELF'], 'index.php') === false) return true;
 		
 		$body = JResponse::getBody();
 		$googlescript = sprintf('
@@ -24,10 +24,10 @@ class plgSystemBlueprogoogleanalytics extends JPlugin {
 				} catch(err) {}
 			</script>', $tracker);
 		
-		$position = strrpos($body, "</body>");
+		$position = utf8_strrpos($body, "</body>");
 		
-		if($position) {
-			$body = substr($body, 0, $position) . $googlescript . substr($body, $position);
+		if ($position) {
+			$body = utf8_substr($body, 0, $position) . $googlescript . utf8_substr($body, $position);
 			JResponse::setBody($body);
 		}
 	}
