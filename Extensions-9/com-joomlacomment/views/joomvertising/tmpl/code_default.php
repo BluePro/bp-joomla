2922a52333cde521130e33119c6a3bc0522070bd
Ondřej Hlaváč <info@bluepro.cz>
2010-05-18 06:23:40 +0000

    Upgrade Phocagallery.

---
+++ Extensions-9/com-joomlacomment/views/joomvertising/tmpl/code_default.php
@@ -0,0 +1,9 @@
+<p class="ads_info">
+    To add advertisement code between the article and the comments, please click <br />
+    on standard banner
+</p>
+<div id="cpanel">
+<?php
+echo $this->quickiconButton( 'index.php?option=com_comment&controller=joomvertising&layout=standard_banner', 'icon-48-config.png', JText::_('Standard banner') );
+?>
+</div>
---
+++ Extensions-9/plg-phocagallery-editor/other/install_xml.php
@@ -0,0 +1,22 @@
+<?php
+/*********** XML PARAMETERS AND VALUES ************/
+$xml_item = "plugin";// component | template
+$xml_file = "plg_editors-xtd_phocagallery";		
+$xml_name = "Button - Phoca Gallery";
+$xml_creation_date = "10/08/2009";
+$xml_author = "Jan Pavelka (www.phoca.cz)";
+$xml_author_email = "";
+$xml_author_url = "www.phoca.cz";
+$xml_copyright = "Jan Pavelka";
+$xml_license = "GNU/GPL";
+$xml_version = "2.5.7";
+$xml_description = "Phoca Gallery Editor Button Plugin - see http://www.phoca.cz for information how to use this plugin";
+$xml_copy_file = 1;//Copy other files in to administration area (only for development), ./front, ./language, ./other
+
+$xml_menu = array();
+$xml_submenu = array();
+
+$xml_install_file = ''; 
+$xml_uninstall_file = '';
+/*********** XML PARAMETERS AND VALUES ************/
+?>
